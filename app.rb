ae60ab3aa0a3c0b6b16bec1497b89db2312228ff
ashantejr <ashantejr@gmail.com>
2016-03-04 18:02:57 +0000

    this is better than the last time

--- app.rb
+++ app.rb
@@ -1,19 +1,20 @@
+# https://www.youtube.com/watch?v=AM-s7CmQ0io&feature=youtu.be
 require_relative 'config/environment'
 
-class App < Sinatra::Base 
+class App < Sinatra::Base
 
   get '/' do
     erb :index
-  end 
+  end
 
 post '/' do
   @user = {
-      name: "#{params[:fname]} #{params[:lname]}", 
+      name: "#{params[:fname]} #{params[:lname]}",
       profilepic: "#{params['propic']}",
       twitter: "#{params['twitter']}",
       facebook: "#{params['facebook']}",
-      google: "#{params['google']}"  
-     } 
+      google: "#{params['google']}"
+     }
     erb :dashboard
   end
 
